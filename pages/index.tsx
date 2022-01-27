4c8da29afd7a37f7c6e71a34dd9b71ba6076fece
fanzru <fattahilaaf080701@gmail.com>
2022-01-27 23:13:01 +0700

    change port and scripts

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="h-screen flex justify-center items-center">
-          Under Development ...
+          Under Development by Fanzru & Revdonz
       </div>
     </>
   )
