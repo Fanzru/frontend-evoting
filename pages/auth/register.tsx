6488dc5b8ba35599ffe85a7e97007c35a555f1f0
RevDonz <aprilio842@gmail.com>
2022-01-29 13:37:21 +0700

    Edit login & register

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,30 +1,29 @@
 import { useRouter } from "next/router";
 
-const register = () => {
+const Register = () => {
   const router = useRouter();
   return (
-    <div className="flex justify-center items-center h-screen sm:bg-gray-100 px-4">
-      <div className="sm:p-5 sm:border-2 rounded-xl bg-white">
-        <div className="flex flex-col">
-          <div className="font-semibold sm:text-xl flex flex-col text-center">
-            <span>Registrasi Pemilih</span>
-          </div>
-          <div className="pt-5">
-            <input type="text" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Email SSO" />
-          </div>
-          <div className="pt-3">
-            <input type="text" name="Password" id="Password" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Password" />
-          </div>
-          <div className="pt-3 flex items-center justify-between">
-            <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500' onClick={() => {router.push("/auth")}}>Kembali</button>
-            <div className="flex items-center space-x-2">
-              <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500'>Register</button>
+    <div className=" sm:bg-gray-100">
+      <div className="flex justify-center items-center h-screen container mx-auto px-4">
+        <div className="sm:p-5 sm:border-2 sm:rounded-xl bg-white w-full md:w-2/3 lg:w-1/2">
+          <div className="flex flex-col">
+            <div className="font-semibold sm:text-xl flex flex-col text-center">
+              <span>Registrasi Pemilih</span>
+            </div>
+            <div className="pt-5">
+              <input type="text" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Email SSO" />
+            </div>
+            <div className="pt-3 flex items-center justify-between">
+              <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500 text-sm md:text-base' onClick={() => {router.push("/auth")}}>Kembali</button>
+              <div className="flex items-center space-x-2">
+                <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500 text-sm md:text-base'>Register</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div> 
+      </div> 
+    </div>
   );
 };
 
-export default register;
+export default Register;
