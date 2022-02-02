0a9e6d7ee6493eb84ef824d5a1a71b2bb891af31
RevDonz <aprilio842@gmail.com>
2022-02-02 14:33:57 +0700

    show/hide password auth

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,34 +1,72 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { HiEye, HiEyeOff } from 'react-icons/hi'
 
 const Login = () => {
-  const router = useRouter();
+  const router = useRouter()
+  const [show, setShow] = useState(false)
+
   return (
     <div className=" sm:bg-gray-100">
-      <div className="flex justify-center items-center h-screen container mx-auto px-4">
-        <div className="sm:p-5 sm:border-2 sm:rounded-xl bg-white w-full md:w-2/3 lg:w-1/2">
+      <div className="container mx-auto flex h-screen items-center justify-center px-4">
+        <div className="w-full bg-white sm:rounded-xl sm:border-2 sm:p-5 md:w-2/3 lg:w-1/2">
           <div className="flex flex-col">
-            <div className="font-semibold sm:text-lg xl:text-xl flex flex-col text-center">
+            <div className="flex flex-col text-center font-semibold sm:text-lg xl:text-xl">
               <span>E-Voting Pemilihan Raya Fakultas XXX dan XXX</span>
               <span>Universitas Telkom 2021</span>
             </div>
             <div className="pt-5">
-              <input type="text" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Email SSO" />
+              <input
+                type="email"
+                className="block w-full rounded-md border border-gray-200 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-400"
+                placeholder="Email SSO"
+              />
             </div>
-            <div className="pt-3">
-              <input type="password" name="Password" id="Password" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Password" />
+            <div className="flex pt-3">
+              <input
+                type={show ? `text` : 'password'}
+                name="Password"
+                id="Password"
+                className="block z-10 w-full rounded-md rounded-r-none border border-gray-200 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-400"
+                placeholder="Password"
+              />
+              <div
+                className="flex items-center justify-center rounded-md rounded-l-none border-y z-0 border-r px-3 py-2 hover:bg-gray-100"
+                onClick={() => {
+                  setShow(!show)
+                }}
+              >
+                {!show ? <HiEye /> : <HiEyeOff />}
+              </div>
             </div>
-            <div className="pt-3 flex items-center justify-between">
-              <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500 text-sm md:text-base' onClick={() => {router.push("/dev")}}>Login</button>
+            <div className="flex items-center justify-between pt-3">
+              <button
+                className="rounded-md bg-gray-400 px-4 py-2 text-sm text-white hover:bg-gray-500 md:text-base"
+                onClick={() => {
+                  router.push('/dev')
+                }}
+              >
+                Login
+              </button>
               <div className="flex items-center space-x-2">
-                <span className='text-gray-400 text-xs md:text-sm'>Belum punya akun?</span>
-                <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500  text-sm md:text-base' onClick={() => {router.push("/auth/register")}}>Register</button>
+                <span className="text-xs text-gray-400 md:text-sm">
+                  Belum punya akun?
+                </span>
+                <button
+                  className="rounded-md bg-gray-400 px-4 py-2 text-sm text-white  hover:bg-gray-500 md:text-base"
+                  onClick={() => {
+                    router.push('/auth/register')
+                  }}
+                >
+                  Register
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </div>  
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,32 +1,63 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
 
 const Register = () => {
   const router = useRouter();
+  const [show, setShow] = useState(false)
+
   return (
     <div className=" sm:bg-gray-100">
-      <div className="flex justify-center items-center h-screen container mx-auto px-4">
-        <div className="sm:p-5 sm:border-2 sm:rounded-xl bg-white w-full md:w-2/3 lg:w-1/2">
+      <div className="container mx-auto flex h-screen items-center justify-center px-4">
+        <div className="w-full bg-white sm:rounded-xl sm:border-2 sm:p-5 md:w-2/3 lg:w-1/2">
           <div className="flex flex-col">
-            <div className="font-semibold sm:text-xl flex flex-col text-center">
+            <div className="flex flex-col text-center font-semibold sm:text-xl">
               <span>Registrasi Pemilih</span>
             </div>
             <div className="pt-5">
-              <input type="text" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Email SSO" />
+              <input
+                type="text"
+                className="block w-full rounded-md border border-gray-200 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-400"
+                placeholder="Email SSO"
+              />
             </div>
-            <div className="pt-3">
-              <input type="password" name="Password" id="Password" className="block w-full px-3 py-2 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" placeholder="Password" />
+            <div className="flex pt-3">
+              <input
+                type={show ? `text` : 'password'}
+                name="Password"
+                id="Password"
+                className="block z-10 w-full rounded-md rounded-r-none border border-gray-200 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-400"
+                placeholder="Password"
+              />
+              <div
+                className="flex z-0 items-center justify-center rounded-md rounded-l-none border px-3 py-2 hover:bg-gray-100"
+                onClick={() => {
+                  setShow(!show)
+                }}
+              >
+                {!show ? <HiEye /> : <HiEyeOff />}
+              </div>
             </div>
-            <div className="pt-3 flex items-center justify-between">
-              <button className='px-4 py-2 rounded-md bg-red-400 text-white hover:bg-red-500 text-sm md:text-base' onClick={() => {router.push("/auth")}}>Kembali</button>
+            <div className="flex items-center justify-between pt-3">
+              <button
+                className="rounded-md bg-red-400 px-4 py-2 text-sm text-white hover:bg-red-500 md:text-base"
+                onClick={() => {
+                  router.push('/auth')
+                }}
+              >
+                Kembali
+              </button>
               <div className="flex items-center space-x-2">
-                <button className='px-4 py-2 rounded-md bg-gray-400 text-white hover:bg-gray-500 text-sm md:text-base'>Register</button>
+                <button className="rounded-md bg-gray-400 px-4 py-2 text-sm text-white hover:bg-gray-500 md:text-base">
+                  Register
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
-  );
+  )
 };
 
 export default Register;
